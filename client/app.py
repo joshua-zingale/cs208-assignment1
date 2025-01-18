08f9a5c2d215c771e2927e6eec1bc437332a35f3
Joshua Zingale <jtzingale@gmail.com>
2025-01-18 00:14:01 +0000

    Added client

---
+++ client/app.py
@@ -0,0 +1,34 @@
+import requests
+import hashlib
+import os
+
+try:
+    URL = os.environ['SERVER_URL']
+except:
+    URL = "http://127.0.0.1:5000"
+
+# Get checksum
+try:
+    checksum = requests.get(f"{URL}/checksum.txt").text
+except Exception as e:
+    print(f"Could not download checksum:")
+    raise e
+
+# Get file
+try:
+    file_text = requests.get(URL).text
+except Exception as e:
+    print(f"Could not download file:")
+    raise e
+
+# Compare file against checksum
+if hashlib.md5(str.encode(file_text)).hexdigest() == checksum:
+
+    # Write file if checksum matches
+    with open("mydata_client_copy.txt", "w") as f:
+        f.write(file_text)
+    print("File has been downloaded, validated, and saved.")
+else:
+    print("Downloaded file did not match the checksome and was discarded.")
+
+
--- server/app.py
+++ server/app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def download_file():
-    return send_file("./mydata.txt", as_attachment = True)
+    return send_file("./mydata.txt", as_attachment = True, download_name="mydata_client_copy.txt")
 
-@app.route("/checksum")
+@app.route("/checksum.txt")
 def download_checksum():
     return send_file("./checksum.txt", as_attachment = True)
